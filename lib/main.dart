62f9ec835408d2384ab8d2ef9bf05116b743bf09
kurjakrichard <kurjak.richard@gmail.com>
2022-07-23 13:01:39 +0200

    Notelist project 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:smartheard/screens/note_detail.dart';
 /*import 'screens/home.dart';
 import 'screens/home2.dart';
 import 'screens/home3.dart';
 import 'screens/home4.dart';
 import 'screens/home5.dart';*/
-import 'screens/note_list.dart';
-import 'screens/home3.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,6 +24,6 @@ class MyApp extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                     primary: Colors.indigo, onPrimary: Colors.white)),
             appBarTheme: const AppBarTheme(color: Colors.indigo)),
-        home: NoteList());
+        home: const NoteDetail());
   }
 }
--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// ignore_for_file: constant_identifier_names
+enum Priorities { High, Low }
+
 class NoteDetail extends StatefulWidget {
   const NoteDetail({Key? key}) : super(key: key);
 
@@ -10,15 +13,102 @@ class NoteDetail extends StatefulWidget {
 }
 
 class NoteDetailState extends State<NoteDetail> {
+  var dropDownItemSelected;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dropDownItemSelected = Priorities.Low;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit note'),
+        title: const Text('Edit note'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
-        child: ListView(),
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            ListTile(
+              title: DropdownButton<Priorities>(
+                value: dropDownItemSelected,
+                items: Priorities.values.map((Priorities dropDownMenuItem) {
+                  return DropdownMenuItem<Priorities>(
+                    value: dropDownMenuItem,
+                    child: Text(dropDownMenuItem.name),
+                  );
+                }).toList(),
+                onChanged: (valueSelectedByUser) {
+                  setState(() {
+                    dropDownItemSelected = valueSelectedByUser;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: titleController,
+                onChanged: (value) {
+                  debugPrint('Valami változott');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: descriptionController,
+                onChanged: (value) {
+                  debugPrint('Valami változott');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text('Save'),
+                      onPressed: () {
+                        setState(() {
+                          debugPrint('Save button clicked!');
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text('Delete'),
+                      onPressed: () {
+                        setState(() {
+                          debugPrint('Delete button clicked!');
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
